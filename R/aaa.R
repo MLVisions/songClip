5759dffdd5db6792f56ee19bd9fe61ad166ad8ab
Kyle Barrett <barrettk@metrumrg.com>
2023-09-08 20:54:49 -0400

    set audio player on load for macos

--- R/aaa.R
+++ R/aaa.R
@@ -1,1 +1,13 @@
 
+# Set audio player on package load. Checks path at '/usr/bin/afplay' (default on macos)
+# Check set path via tuneR::getWavPlayer()
+#
+# TODO: add support for Windows
+set_audio_player <- function(){
+  player <- '/usr/bin/afplay'
+  if(fs::file_exists(player)){
+    tuneR::setWavPlayer(player)
+  }
+}
+
+set_audio_player()
