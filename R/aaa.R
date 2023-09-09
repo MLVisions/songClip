3212f215f98343fccd94631564f8c9dacc3cb77b
Kyle Barrett <barrettk@metrumrg.com>
2023-09-08 21:07:52 -0400

    improve set_audio_player on load

--- R/aaa.R
+++ R/aaa.R
@@ -5,8 +5,12 @@
 # TODO: add support for Windows
 set_audio_player <- function(){
   player <- '/usr/bin/afplay'
-  if(fs::file_exists(player)){
+  if(.Platform$OS.type == "unix" && fs::file_exists(player)){
     tuneR::setWavPlayer(player)
+  }else{
+    cli::cli_div(theme = list(span.emph = list(color = "red")))
+    cli::cli_warn(c("!" = "{.emph Audio player not set}. Please set the path to your audio player via:",
+                    ">" = "{.code tuneR::setWavPlayer('path/to/player')}"))
   }
 }
 
