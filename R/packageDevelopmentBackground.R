efde9ec4bb6eb9fc1f83cf39718ee9afd9174902
Kyle Barrett <barrettk@metrumrg.com>
2023-09-10 14:18:47 -0400

    update background with more info

--- R/packageDevelopmentBackground.R
+++ R/packageDevelopmentBackground.R
@@ -1,14 +1,55 @@
 
 
+# Useful Commands ---------------------------------------------------------
+
+# Sections like the one above can be created with `CMD + SHIFT + r`
+
+# `CMD + f` will search the script
+
+# When in an R project, `CMD + SHIFT + f` will search the entire R project for that file
+#   - you can choose to just search a directory too
+
+# `CMD + Enter` (or `CMD + return`), will run either A) the current call your cursor is at, or
+# B) run what you have highlighted
+
+
+# If you hold `control + option`, and then it the up or down arrow, you can edit multiple lines at once
+# (try it, it's cool)
+
+
 
 # Background & Best Practices for Package Development ---------------------
 
 
 
-# Loading all functions and variables defined in R/ directory:
+
+
+# Loading all functions and variables defined in R/ directory during package development:
 
 # `devtools::load_all()`
 
+# Loading exported functions from a package you installed:
+
+# library(dplyr)
+# library(tuneR)
+# library(songClip)
+
+# Notes:
+# `library()` calls are used when scripting, such as for specific client projects, modeling project,
+# shiny app, Rmarkdown, etc.
+# However in most circumstances you *dont* want `library()` calls in an R package. Instead,
+# you either A) import the package functions you need and it to the `NAMESPACE` (more on this later), or
+# B) use qualifiers (<package_name>::<package function>, e.g, `tuneR::readMP3()`)
+
+# Packages that are needed for the package, are added to the `Imports` section of the `DESCRIPTION` file.
+
+# The `DESCRIPTION`, `NAMESPACE`, and `.Rbuildignore` files are the 3 files that decide how an R package
+# gets installed
+
+# The `.Rprofile` sets the development environment. It can be used for all the scripting examples, as well
+# as an R package development environment. You often use one to set `options()` or for static file paths
+# (e.g., a path to an installation or tarball).
+# Here, we are using it to set where to grab the packages from.
 
 
 
@@ -33,7 +74,8 @@
 # Example:
 # library(dplyr)
 # data(mtcars)
-# mtcars %>% mutate(new_column = gear + 2) # point being, you didnt have to write `dplyr::mutate()` because you called the library
+# mtcars %>% mutate(new_column = gear + 2) # point being, you didnt have to write `dplyr::mutate()` because you
+# *called the library* via `library()`
 
 # However in R package development, we *only* have access to the specific functions we *import* when we go to
 # *release* a package. The `main` branch is meant to signify a state where the code works, but doesnt have to match the latest release.
@@ -99,10 +141,21 @@
 #' append it to the front of the dataframe.
 #' @param group_by_col column to group by if creating an ID column.
 #'
+#' @importFrom rlang syms sym
+#' @importFrom dplyr n
+#'
 #' @details
 #' This is a terrible, disgusting function. Why would you ever want to do this.
 #' Im mainly trying to illustrate the different types of arguments/defaults you
 #' can have, and how roxygen/the loaded function will work.
+#'
+#' The `@importFrom` statements above will not show up in the helper file (this text will),
+#' however the `NAMESPACE` file will update when you run `devtools::document()` if you modify them.
+#'
+#' The `@importFrom` statements above translate to:
+#' - "import `syms` and `sym` from the package `rlang`
+#' - "import `n` from the package `dplyr`
+#'
 #'
 #' **bolded** word
 #'
@@ -112,7 +165,7 @@
 #'
 #' This section will be a code block:
 #' ```
-#' # Adds a new column to a dataset, repeating 'A' all the way down:
+#' # Adds a new column to a dataset, repeating 'A' all the way down (see ?dplyr::mutate for more details):
 #' data %>% dplyr::mutate(new_column = "A")
 #' ```
 #'
@@ -139,8 +192,6 @@
 #' my_function(symbol = "c")
 #' }
 #'
-#' @importFrom rlang syms sym
-#' @importFrom dplyr n
 #'
 #' @returns a dataframe
 #'
