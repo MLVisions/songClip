ce90ad9efb51dda46c96966d6763122ea8e3a0ba
Kyle Barrett <barrettk@metrumrg.com>
2024-02-10 11:31:16 -0500

    remove python functions

--- R/aaa.R
+++ R/aaa.R
@@ -52,28 +52,6 @@ get_audio_examples <- function() {
   list.files(example_dir)
 }
 
-# Make sure python is set up ----------------------------------------------
-
-python_is_installed <- function(use_environment = NULL){
-  config <- tryCatch({
-    reticulate::py_discover_config(use_environment = use_environment)
-    }, error = identity)
-
-  if(inherits(config, "error")){
-    FALSE
-  }else{
-    fs::file_exists(config$python)
-  }
-}
-
-is_miniconda_installed <- function(path = reticulate::miniconda_path()){
-  exe <- if (xfun::is_windows()){
-    "condabin/conda.bat"
-  }else{
-    "bin/conda"
-  }
-  file.path(path, exe)
-}
 
 
 # Make sure audio player is set up ----------------------------------------
