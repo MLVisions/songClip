8ab3b0a7a199fb746ab5f61b8f7fc2dd2262a2a5
Kyle Barrett <barrettk@metrumrg.com>
2024-02-10 10:43:46 -0500

    remove python code
     - A) not used, and likely wont be
     - B) moved python code to [pythonR](https://barrettk.github.io/pythonR/)

--- tests/testthat/test-python-setup.R
+++
@@ -1,9 +0,0 @@
-
-testthat::test_that("python setup", {
-  skip_if_no_python()
-  py_env <- tryCatch({
-    setup_py_env(py_pkgs = c("pandas", "numpy", "scipy"))
-  }, error = identity)
-
-  testthat::expect_true(is.list(py_env))
-})
